ecff9012fe7a86c31e93880dce9312088f84f76e
Manuel Duarte <carlosd99m@gmail.com>
2021-10-16 23:57:54 -0600

    add CI file

--- app/src/main/java/com/manuelduarte077/cleanarchitecturenoteapp/MainActivity.kt
+++ app/src/main/java/com/manuelduarte077/cleanarchitecturenoteapp/MainActivity.kt
@@ -3,15 +3,30 @@ package com.manuelduarte077.cleanarchitecturenoteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.manuelduarte077.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CleanArchitectureNoteAppTheme {
-                
+                SimpleComposable()
             }
         }
     }
+}
+
+
+@Composable
+fun SimpleComposable() {
+    Text("Hello World")
+}
+@Preview
+@Composable
+fun ComposablePreview() {
+    SimpleComposable()
 }
