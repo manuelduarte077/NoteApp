88e65d06862c1678f5ceb525fb5a48352ed59e1c
Manuel Duarte  <carlosd99m@gmail.com>
2023-04-02 11:46:04 -0600

    add fonts

--- app/src/main/java/com/manuelduarte077/noteapp/ui/theme/Type.kt
+++ app/src/main/java/com/manuelduarte077/noteapp/ui/theme/Type.kt
@@ -2,9 +2,25 @@ package com.manuelduarte077.noteapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.manuelduarte077.noteapp.R
+
+
+val RedHatDisplay = FontFamily(
+
+    Font(R.font.redhatdisplay_regular, FontWeight.Normal),
+    Font(R.font.redhatdisplay_medium, FontWeight.Medium),
+    Font(R.font.redhatdisplay_semibold, FontWeight.SemiBold),
+    Font(R.font.redhatdisplay_bold, FontWeight.Bold),
+    Font(R.font.redhatdisplay_italic, FontWeight.Normal, style = FontStyle.Italic),
+
+
+    )
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
